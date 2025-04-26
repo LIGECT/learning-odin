de6b9ae61a111bd93a3b776a2da06c6d04c2f671
LIGECT <viminiko@gmail.com>
2025-04-26 19:09:42 +0300

    Day 85. Split join practice

--- odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
+++ odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
@@ -1,29 +1,21 @@
-const users = [
-  { id: 101, name: "Алиса", isActive: true, age: 25 },
-  { id: 102, name: "Боб", isActive: false, age: 30 },
-  { id: 103, name: "Чарли", isActive: true, age: 22 },
-  { id: 104, name: "Дэвид", isActive: true, age: 40 },
-  { id: 105, name: "Ева", isActive: false, age: 28 },
-];
+const sentence = "Это просто тестовое предложение для практики split";
+const csvData = "яблоко, банан, апельсин, виноград";
+const word = "Программирование";
 
-let nameUsers = users.map((item) => item.name);
-console.log(nameUsers);
+let arr = sentence.split(" ");
+console.log(arr);
 
-let userList = users.map((item) => `Пользователь: ${item.name}`);
-console.log(userList);
+let newArrJoin = arr.join("-");
+console.log(newArrJoin);
 
-let emailList = users.map(
-  (item) => `${item.name.toLowerCase()}_${item.age}@example.com`
-);
-console.log(emailList);
+let newCsvData = csvData.split(", ").join(" ");
+console.log(newCsvData);
 
-let statusList = users.map((user) => {
-  let statusValue = user.isActive ? "Активен" : "Неактивен";
+let newWord = word.split("");
+console.log(newWord);
 
-  return {
-    ...user,
-    status: statusValue,
-  };
-});
+let joinWord = newWord.join("");
+console.log(joinWord);
 
-console.log(statusList);
+let reverseWord = word.split("").reverse().join("");
+console.log(reverseWord);
