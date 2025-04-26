ddbaa45b30171bbd7fe4db7f9df2e7d925d0e486
LIGECT <viminiko@gmail.com>
2025-04-26 20:30:07 +0300

    Day 85. Practicing tasks on reduce and reduceRight

--- odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
+++ odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
@@ -1,21 +1,39 @@
-const sentence = "Это просто тестовое предложение для практики split";
-const csvData = "яблоко, банан, апельсин, виноград";
-const word = "Программирование";
+const numbers = [10, -5, 0, 25, -15, 30, 8, 0, 40];
+const users = [
+  { id: 101, name: "Алиса", isActive: true, age: 25 },
+  { id: 102, name: "Боб", isActive: false, age: 30 },
+  { id: 103, name: "Чарли", isActive: true, age: 22 },
+  { id: 104, name: "Дэвид", isActive: true, age: 40 },
+  { id: 105, name: "Ева", isActive: false, age: 28 },
+];
 
-let arr = sentence.split(" ");
-console.log(arr);
+// let result = numbers.reduce((acc, curr) => acc + curr, 0);
+// console.log(result);
 
-let newArrJoin = arr.join("-");
-console.log(newArrJoin);
+// let returnMax = numbers.reduce((acc, curr) => {
+//   return Math.max(acc, curr);
+// });
+// console.log(returnMax);
 
-let newCsvData = csvData.split(", ").join(" ");
-console.log(newCsvData);
+// let activeNotActive = users.reduce(
+//   (counts, user) => {
+//     if (user.isActive === true) {
+//       counts.active++;
+//     } else {
+//       counts.inactive++;
+//     }
 
-let newWord = word.split("");
-console.log(newWord);
+//     return counts;
+//   },
+//   {
+//     active: 0,
+//     inactive: 0,
+//   }
+// );
 
-let joinWord = newWord.join("");
-console.log(joinWord);
+// console.log(activeNotActive);
 
-let reverseWord = word.split("").reverse().join("");
-console.log(reverseWord);
+let newReduce = users.reduceRight((accumulator, current) => {
+  return accumulator === "" ? current.name : current.name + ", " + accumulator;
+}, "");
+console.log(newReduce);
