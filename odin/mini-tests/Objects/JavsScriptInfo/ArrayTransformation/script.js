6bedde0b7f557851ae20e46923a3a1e966856923
LIGECT <viminiko@gmail.com>
2025-04-28 11:42:41 +0300

    Day 87. Solve the range filtering problem

--- odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
+++ odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
@@ -1,39 +1,15 @@
-const numbers = [10, -5, 0, 25, -15, 30, 8, 0, 40];
-const users = [
-  { id: 101, name: "Алиса", isActive: true, age: 25 },
-  { id: 102, name: "Боб", isActive: false, age: 30 },
-  { id: 103, name: "Чарли", isActive: true, age: 22 },
-  { id: 104, name: "Дэвид", isActive: true, age: 40 },
-  { id: 105, name: "Ева", isActive: false, age: 28 },
-];
+let arr = [5, 3, 8, 1];
 
-// let result = numbers.reduce((acc, curr) => acc + curr, 0);
-// console.log(result);
+let filtered = filterRange(arr, 1, 4);
 
-// let returnMax = numbers.reduce((acc, curr) => {
-//   return Math.max(acc, curr);
-// });
-// console.log(returnMax);
+function filterRange(arr, a, b) {
+  const filtered = arr.filter((number) => {
+    return number >= a && number <= b;
+  });
 
-// let activeNotActive = users.reduce(
-//   (counts, user) => {
-//     if (user.isActive === true) {
-//       counts.active++;
-//     } else {
-//       counts.inactive++;
-//     }
+  return filtered;
+}
 
-//     return counts;
-//   },
-//   {
-//     active: 0,
-//     inactive: 0,
-//   }
-// );
+console.log(filtered);
 
-// console.log(activeNotActive);
-
-let newReduce = users.reduceRight((accumulator, current) => {
-  return accumulator === "" ? current.name : current.name + ", " + accumulator;
-}, "");
-console.log(newReduce);
+console.log(arr);
