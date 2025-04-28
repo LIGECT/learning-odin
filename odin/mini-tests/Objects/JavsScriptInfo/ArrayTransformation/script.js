c1ba826090593bd702cd1a6746e0c488290b1619
LIGECT <viminiko@gmail.com>
2025-04-28 16:38:01 +0300

    Day 87. Transform into an array of names

--- odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
+++ odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
@@ -1,39 +1,10 @@
-class Calculator {
-  constructor() {
-    //code
-    this.methods = {
-      "+": (a, b) => a + b,
-      "-": (a, b) => a - b,
-    };
-  }
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
 
-  calculate(str) {
-    let parts = str.split(" ");
-    let a = +parts[0];
-    let b = +parts[2];
-    let operator = parts[1];
-    let operationFunc = this.methods[operator];
+let users = [vasya, petya, masha];
 
-    if (!operationFunc) {
-      throw new Error(`Оператор "${operator}" не найден`);
-    }
-
-    return operationFunc(a, b);
-  }
-
-  addMethod(name, func) {
-    //code
-
-    this.methods[name] = func;
-  }
-}
-
-let calc = new Calculator();
-
-console.log(calc.calculate("3 + 7"));
-let powerCalc = new Calculator();
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
-console.log(powerCalc.calculate("2 ** 3"));
-console.log(powerCalc.calculate("10 * 5"));
+let names =
+  /* ... ваш код */
+  users.map((user) => user.name);
+console.log(names); // Вася, Петя, Маша
