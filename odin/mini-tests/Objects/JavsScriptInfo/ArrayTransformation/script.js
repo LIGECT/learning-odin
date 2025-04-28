dc3c25f3d85bd8861a97a717065d320ceeb7223c
LIGECT <viminiko@gmail.com>
2025-04-28 19:08:40 +0300

    Day 87. Sort users by age

--- odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
+++ odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
@@ -1,21 +1,14 @@
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
 
-let users = [vasya, petya, masha];
+let arr = [vasya, petya, masha];
 
-let usersMapped = users.map((user) => ({
-  fullName: user.name + " " + user.surname,
-  id: user.id,
-}));
+function sortByAge(arr) {
+  arr.sort((a, b) => a.age - b.age);
+}
+sortByAge(arr);
 
-/*
-usersMapped = [
-  { fullName: "Вася Пупкин", id: 1 },
-  { fullName: "Петя Иванов", id: 2 },
-  { fullName: "Маша Петрова", id: 3 }
-]
-*/
-
-console.log(usersMapped[0].id); // 1
-console.log(usersMapped[0].fullName); // Вася Пупкин
+console.log(arr[0].name); // Ожидаем Вася
+console.log(arr[1].name); // Ожидаем Маша
+console.log(arr[2].name); // Ожидаем Петя
