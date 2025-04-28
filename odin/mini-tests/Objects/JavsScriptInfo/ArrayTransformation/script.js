a3aa039aa397e9b5ff7c8feb16a022c12f3f73c9
LIGECT <viminiko@gmail.com>
2025-04-28 11:59:26 +0300

    Day 87. Solve the problem Range Filtering in Place

--- odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
+++ odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
@@ -1,15 +1,13 @@
 let arr = [5, 3, 8, 1];
 
-let filtered = filterRange(arr, 1, 4);
-
-function filterRange(arr, a, b) {
-  const filtered = arr.filter((number) => {
-    return number >= a && number <= b;
-  });
-
-  return filtered;
+function filterRangeInPlace(arr, a, b) {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1);
+    }
+  }
 }
 
-console.log(filtered);
+filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
 
 console.log(arr);
