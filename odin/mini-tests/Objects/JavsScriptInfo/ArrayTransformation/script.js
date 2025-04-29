f15574aa78f6e4f2db3779850e06c3b508095842
LIGECT <viminiko@gmail.com>
2025-04-29 19:17:11 +0300

    Day 88. Keep unique elements of the array

--- odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
+++ odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
@@ -1,13 +1,17 @@
-function getAverageAge(users) {
-  //code
-  let sumUserAge = users.reduce((sum, user) => sum + user.age, 0);
-  return sumUserAge / users.length;
+function unique(arr) {
+  set = new Set(arr);
+  return [...set];
 }
+let strings = [
+  "кришна",
+  "кришна",
+  "харе",
+  "харе",
+  "харе",
+  "харе",
+  "кришна",
+  "кришна",
+  ":-O",
+];
 
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 29 };
-
-let arr = [vasya, petya, masha];
-
-console.log(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
+console.log(unique(strings)); // Ожидаем кришна, харе, :-O
