fec12ed077023b95f3ec33ac0e0080d124ccb0f2
LIGECT <viminiko@gmail.com>
2025-04-29 19:37:44 +0300

    Day 88: Create an object with keys from an array

--- odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
+++ odin/mini-tests/Objects/JavsScriptInfo/ArrayTransformation/script.js
@@ -1,17 +1,17 @@
-function unique(arr) {
-  set = new Set(arr);
-  return [...set];
-}
-let strings = [
-  "кришна",
-  "кришна",
-  "харе",
-  "харе",
-  "харе",
-  "харе",
-  "кришна",
-  "кришна",
-  ":-O",
+let users = [
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
 ];
 
-console.log(unique(strings)); // Ожидаем кришна, харе, :-O
+function groupById(arr) {
+  return arr.reduce((obj, user) => {
+    obj[user.id] = user;
+    return obj;
+  }, {});
+}
+
+let usersById = groupById(users);
+
+console.log(usersById);
+console.log(usersById["john"].name);
