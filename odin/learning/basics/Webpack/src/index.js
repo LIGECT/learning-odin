266d3f348d206a7f41e4b05381c1d2ccacba0f52
LIGECT <viminiko@gmail.com>
2025-08-07 20:56:43 +0300

    Day 187. Webpack Practice

---
+++ odin/learning/basics/Webpack/src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { greeting } from "./greeting";
+// import OdinImage from "./odin.png";
+
+// const image = document.createElement("img");
+// image.src = OdinImage;
+
+// document.body.appendChild(image);
+
+console.log(greeting);
